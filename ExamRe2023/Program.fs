fc7ff6b16b356e05449d06eedad7d8b2ce408f3f
AlbertRossJoh <albertrossj@gmail.com>
2024-05-24 19:52:25 +0200

    arithmatic completed

--- ExamRe2023/Program.fs
+++ ExamRe2023/Program.fs
@@ -1,2 +1,52 @@
-﻿// For more information see https://aka.ms/fsharp-console-apps
-printfn "Hello from F#"
+﻿open System
+open ReExam2023
+
+let testQ1 () =
+    (* Testsfor Q1.1 *)
+    printfn "Testing Question 1"
+    // printfn "%A" (eval p1)
+    // printfn "%A" (eval p2)
+    // printfn "%A" (eval p3)
+    // printfn "%A" (negate p1)
+    // printfn "%A" (negate p2)
+    // printfn "%A" (negate p3)
+    // printfn "%A" (subtract p1 p2)
+    // printfn "%A" (subtract p2 p3)
+    // printfn "%A" (subtract p3 p1)
+    // printfn "%A" (subtract p3 p1 |> eval)
+    // printfn "%A" (multiply p1 p1)
+    // printfn "%A" (multiply p2 p2)
+    // printfn "%A" (multiply p3 p3)
+    // printfn "%A" (pow (arith.Num 2) (arith.Num 8))
+    // printfn "%A" (pow p2 (arith.Num 3))
+    // printfn "%A" (iterate negate p1 p2)
+    // printfn "%A" (iterate (sprintf "f(%s)") "acc" p2)
+    // printfn "%A" (iterate negate p1 (subtract p2 (arith.Num 1)))
+    // printfn "%A" (pow2 (arith.Num 2) (arith.Num 8))
+    // printfn "%A" (pow2 p2 (arith.Num 3) |> eval)
+    ()
+    
+
+let testQ2 () =
+    printfn "Testing Question 2"
+    // place debug prints for Q2 here
+    ()
+
+let testQ3 () =
+    printfn "Testing Question 3"
+    // place debug prints for Q3 here
+    ()
+
+let testQ4 () =
+    printfn "Testing Question 4"
+    // place debug prints for Q4 here
+    ()
+
+
+open JParsec.TextParser
+
+[<EntryPoint>]
+let main argv =
+    // testQ1 ()
+  
+    0 // return an integer exit code
